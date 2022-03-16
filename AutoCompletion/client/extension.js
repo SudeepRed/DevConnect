a58d8596339641a8e7e7b37662cd29c26414fad2
expnoob <sudeep19313@iiitd.ac.in>
2022-03-16 18:06:16 +0530

    Updated File Structure

--- AutoCompletion/client/extension.js
+++ AutoCompletion/client/extension.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const path = require("path");
 const node_1 = require("vscode-languageclient/node");
+console.log("Client");
 let client;
 function activate(context) {
     let serverPath = context.asAbsolutePath(path.join("server", "server.js"));
